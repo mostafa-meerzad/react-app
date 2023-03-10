0401ae5b8a9a9b4e1f99161f907b0ec4878875af
mostafa <mostafameerzad@gmail.com>
2023-03-10 21:16:20 +0430

    Complete a basic logic for timer

--- src/timer/Timer.jsx
+++ src/timer/Timer.jsx
@@ -1,44 +1,90 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 function Timer() {
-// console.log(1.4.toFixed(1));
-  const [timer, setTimer] = useState(110)
+  const [timer, setTimer] = useState(110);
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [timerId, setTimerId] = useState(Infinity);
+  
+  const [sessionLen, setSessionLen] = useState(0);
+  const [breakLen, setBreakLen] = useState(0);
 
-  const [sessionLen, setSessionLen] = useState(0)
-  const [breakLen, setBreakLen] = useState(0)
-
+  const [timerType, setTimerType] = useState("session");
 
   const handleTimer = () => {
-    console.log("timer called");
-  }
+    if (!isTimerRunning) {
+      setIsTimerRunning(true);
+      if (timerType === "session") {
+        setTimer(sessionLen)
+        setTimerId(
+          setInterval(() => {
+            if(timer > 0 ){
+              setTimer((prevState) => prevState - 1);
+            } else{
+              setTimerType("break");
+            }
+          }, 500)
+        );
+      } else{
+        setTimer(breakLen)
+        setTimerId(
+          setInterval(() => {
+            if(timer > 0 ){
+              setTimer((prevState) => prevState - 1);
+            } else{
+              setTimerType("session");
+            }
+          }, 500)
+        )
+      }
+    } else {
+      setIsTimerRunning(false);
+      clearInterval(timerId);
+    }
+  };
   return (
     <div>
       <h3>seconds {timer % 60}</h3>
       <h3>minutes {Math.floor(timer / 60)}</h3>
       <br />
+      <h4>timer type: {timerType}</h4>
       <br />
-      <button onClick={() => {
-        setSessionLen(prevState=>prevState + 1)
-      }}>session length + </button>
-      <button onClick={() => {
-        setSessionLen(prevState=>prevState - 1)
-      }}>session length - </button>
+      <button
+        onClick={() => {
+          setSessionLen((prevState) => prevState + 60);
+        }}
+      >
+        session length +{" "}
+      </button>
+      <button
+        onClick={() => {
+          setSessionLen((prevState) => prevState - 60);
+        }}
+      >
+        session length -{" "}
+      </button>
       <br />
       <h4>session length {sessionLen}</h4>
       <h4>break length {breakLen}</h4>
 
       <br />
       <br />
-      <button onClick={() => {
-        setBreakLen(prevState=>prevState + 1)
-      }}>break length + </button>
-      <button onClick={() => {
-        setBreakLen(prevState=>prevState + 1)
-      }}>break length - </button>
-       <button onClick={handleTimer}>start</button>
+      <button
+        onClick={() => {
+          setBreakLen((prevState) => prevState + 60);
+        }}
+      >
+        break length +{" "}
+      </button>
+      <button
+        onClick={() => {
+          setBreakLen((prevState) => prevState + 60);
+        }}
+      >
+        break length -{" "}
+      </button>
+      <button onClick={handleTimer}>start</button>
     </div>
-
-  )
+  );
 }
 
-export default Timer
+export default Timer;
